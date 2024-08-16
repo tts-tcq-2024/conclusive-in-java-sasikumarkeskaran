2dcb8b5d14fc2bdbe031ae5c846db95c37c56664
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-08-16 12:00:08 +0530

    Update TemperatureClassifier .java

--- src/main/java/TypewiseAlert/TemperatureClassifier .java
+++ src/main/java/TypewiseAlert/TemperatureClassifier .java
@@ -5,7 +5,18 @@ public class TemperatureClassifier {
     private final BreachTypeChecker breachTypeChecker = new BreachTypeChecker();
 
     public BreachTypeChecker.BreachType classifyTemperatureBreach(
+            CoolingTypeLimits.CoolingType coolingTypepackage TypewiseAlert;
+
+public class TemperatureClassifier {
+
+    private final BreachTypeChecker breachTypeChecker = new BreachTypeChecker();
+
+    public BreachTypeChecker.BreachType classifyTemperatureBreach(
             CoolingTypeLimits.CoolingType coolingType, double temperatureInC) {
+        return breachTypeChecker.inferBreach(temperatureInC, coolingType.getLowerLimit(), coolingType.getUpperLimit());
+    }
+}
+, double temperatureInC) {
         return breachTypeChecker.inferBreach(temperatureInC, coolingType.getLowerLimit(), coolingType.getUpperLimit());
     }
 }
