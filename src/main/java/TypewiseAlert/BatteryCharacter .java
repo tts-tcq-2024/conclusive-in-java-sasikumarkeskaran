65b3eba9915209c754d3be25abe4c2c48710f418
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-08-16 11:23:06 +0530

    Create BatteryCharacter .java

---
+++ src/main/java/TypewiseAlert/BatteryCharacter .java
@@ -0,0 +1,11 @@
+package TypewiseAlert;
+
+public class BatteryCharacter {
+    public CoolingTypeLimits.CoolingType coolingType;
+    public String brand;
+
+    public BatteryCharacter(CoolingTypeLimits.CoolingType coolingType, String brand) {
+        this.coolingType = coolingType;
+        this.brand = brand;
+    }
+}
