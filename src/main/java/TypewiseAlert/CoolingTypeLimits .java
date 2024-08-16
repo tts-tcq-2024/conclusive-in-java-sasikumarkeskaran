0edd02e21fd52f0a10476a3951ba26a327fc5347
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-08-16 12:00:43 +0530

    Update CoolingTypeLimits .java

--- src/main/java/TypewiseAlert/CoolingTypeLimits .java
+++ src/main/java/TypewiseAlert/CoolingTypeLimits .java
@@ -3,7 +3,33 @@ package TypewiseAlert;
 public class CoolingTypeLimits {
 
     public enum CoolingType {
+        PASSIVE_COOLING(0, 35),package TypewiseAlert;
+
+public class CoolingTypeLimits {
+
+    public enum CoolingType {
         PASSIVE_COOLING(0, 35),
+        HI_ACTIVE_COOLING(0, 45),
+        MED_ACTIVE_COOLING(0, 40);
+
+        private final int lowerLimit;
+        private final int upperLimit;
+
+        CoolingType(int lowerLimit, int upperLimit) {
+            this.lowerLimit = lowerLimit;
+            this.upperLimit = upperLimit;
+        }
+
+        public int getLowerLimit() {
+            return lowerLimit;
+        }
+
+        public int getUpperLimit() {
+            return upperLimit;
+        }
+    }
+}
+
         HI_ACTIVE_COOLING(0, 45),
         MED_ACTIVE_COOLING(0, 40);
 
