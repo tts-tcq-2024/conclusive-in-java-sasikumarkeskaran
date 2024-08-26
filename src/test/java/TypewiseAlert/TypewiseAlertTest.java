253f5a5520d245437e69045671506eba87f204f1
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-08-26 14:25:49 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -11,37 +11,37 @@ public class TypewiseAlertTest {
 
     @Test
     public void testInferBreachTooLow() {
-        BreachType breachType = BreachClassifier.inferBreach(10, 20, 30);
+        BreachType breachType = BreachTypeChecker.inferBreach(10, 20, 30);
         assertEquals(BreachType.TOO_LOW, breachType);
     }
 
     @Test
     public void testInferBreachTooHigh() {
-        BreachType breachType = BreachClassifier.inferBreach(40, 20, 30);
+        BreachType breachType = BreachTypeChecker.inferBreach(40, 20, 30);
         assertEquals(BreachType.TOO_HIGH, breachType);
     }
 
     @Test
     public void testInferBreachNormal() {
-        BreachType breachType = BreachClassifier.inferBreach(25, 20, 30);
+        BreachType breachType = BreachTypeChecker.inferBreach(25, 20, 30);
         assertEquals(BreachType.NORMAL, breachType);
     }
 
     @Test
     public void testTempBreachForPassiveCooling() {
-        BreachType breachType = BreachClassifier.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 36);
+        BreachType breachType = BreachTypeChecker.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 36);
         assertEquals(BreachType.TOO_HIGH, breachType);
     }
 
     @Test
     public void testTempBreachForHiActiveCooling() {
-        BreachType breachType = BreachClassifier.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 46);
+        BreachType breachType = BreachTypeChecker.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 46);
         assertEquals(BreachType.TOO_HIGH, breachType);
     }
 
     @Test
     public void testTempBreachForMedActiveCooling() {
-        BreachType breachType = BreachClassifier.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 41);
+        BreachType breachType = BreachTypeChecker.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 41);
         assertEquals(BreachType.TOO_HIGH, breachType);
     }
 
